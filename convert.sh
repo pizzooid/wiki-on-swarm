976d6315692eebfd92b96672c61937cfd8b3bcb6
Pietro <pietro@palmesi.com>
2022-03-19 16:13:37 +0200

    update convert script

--- convert.sh
+++ convert.sh
@@ -1,11 +1,12 @@
 ln -s /zim /zimbee/dump
 for f in /zim/*.zim ; do 
-dir="${f%.zim}" 
-# zimdump dump "$f" --dir "$dir"; 
-cd /zimbee
-npm run createIndex "$dir"
-cd /zimbee/zimbee-frontend
-pwd
-mkdir -p "$dir/frontend"
-./node_modules/.bin/vite build --outDir "$dir/X" --emptyOutDir
+  dir="${f%.zim}" 
+  echo "Extracting $dir with zimdump"
+  zimdump dump "$f" --dir "$dir"; 
+  cd /zimbee
+  npm run createIndex "$dir"
+  cd /zimbee/zimbee-frontend
+  pwd
+  mkdir -p "$dir/frontend"
+  ./node_modules/.bin/vite build --outDir "$dir/X" --emptyOutDir
 done
